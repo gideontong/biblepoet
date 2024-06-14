8ebe1734c01151331010ac6cb88cfd8e15dd5402
Gideon Tong <gtong@cloudflare.com>
2024-06-13 23:44:01 -0700

    Add indent of two spaces (#28)
    * Fix dependency issues
    
    * Add indent of 2 spaces

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylisticTs from '@stylistic/eslint-plugin-ts';
+import parserTs from '@typescript-eslint/parser';
+
+
+export default [
+  {
+    plugins: {
+      '@stylistic/ts': stylisticTs
+    },
+    rules: {
+      '@stylistic/ts/indent': ['error', 2]
+    }
+  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node, parser: parserTs } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
