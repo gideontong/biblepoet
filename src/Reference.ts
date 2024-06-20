f6bc18fef88070217efdffbe7ca125c1cd87e7fa
Nathan Sit <nsit.earth@gmail.com>
2024-06-19 20:44:04 -0700

    fix lint

--- src/Reference.ts
+++ src/Reference.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-used-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class Reference {
-	constructor() { }
+  constructor() {}
 }
