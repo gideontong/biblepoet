84604c7537e4fa0ec3001e8f2139fd267e2a0066
Nathan Sit <nsit.earth@gmail.com>
2024-06-19 20:46:24 -0700

    fix lint

--- src/Version.ts
+++ src/Version.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-used-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class Version {
-	constructor() { }
+  constructor() {}
 }
