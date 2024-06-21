66486b873898547be51b3fa497d154f19b0a60e5
Gideon Tong <gtong@cloudflare.com>
2024-06-21 22:30:34 +0000

    Add book name to Book class (#13)

--- src/Book.ts
+++ src/Book.ts
@@ -1,7 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class Book {
+  name: string;
+  chapterCount: number;
   prev: Book;
   next: Book;
-  chapterCount: number;
   constructor() { }
 }
