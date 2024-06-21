c64c3f55bc4c164b531e1384ad8519fa2e103609
Gideon Tong <gtong@cloudflare.com>
2024-06-21 22:37:05 +0000

    Add tag for secondary names of Book (#19)

--- src/Book.ts
+++ src/Book.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class Book {
   name: string;
+  // TODO: Only allow valid languages
+  otherNames: Map<string, string>;
   chapterCount: number;
   prev: Book;
   next: Book;
