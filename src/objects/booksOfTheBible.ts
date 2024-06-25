f423921a3cf01d7536956bda43079bef31ee6b93
Nathan Sit <nsit.earth@gmail.com>
2024-06-24 23:54:45 -0700

    added booksOfTheBible

---
+++ src/objects/booksOfTheBible.ts
@@ -0,0 +1,82 @@
+import Book from "../classes/Book";
+
+const Genesis = new Book();
+const Exodus = new Book();
+const Leviticus = new Book();
+const Numbers = new Book();
+const Deuteronomy = new Book();
+
+const Joshua = new Book();
+const Judges = new Book();
+const Ruth = new Book();
+const _1Samuel = new Book();
+const _2Samuel = new Book();
+
+const _1Kings = new Book();
+const _2Kings = new Book();
+const _1Chronicles = new Book();
+const _2Chronicles = new Book();
+const Ezra = new Book();
+
+const Nehemiah = new Book();
+const Esther = new Book();
+const Job = new Book();
+const Psalms = new Book();
+const Proverbs = new Book();
+
+const Ecclesiastes = new Book();
+const SongOfSongs = new Book();
+const Isaiah = new Book();
+const Jeremiah = new Book();
+const Lamentations = new Book();
+
+const Ezekiel = new Book();
+const Daniel = new Book();
+const Hosea = new Book();
+const Joel = new Book();
+const Amos = new Book();
+
+const Obadiah = new Book();
+const Jonah = new Book();
+const Micah = new Book();
+const Nahum = new Book();
+const Habakkuk = new Book();
+
+const Zephaniah = new Book();
+const Haggai = new Book();
+const Zechariah = new Book();
+const Malachi = new Book();
+
+// New Testament Books
+const Matthew = new Book();
+const Mark = new Book();
+const Luke = new Book();
+const John = new Book();
+
+const Acts = new Book();
+const Romans = new Book();
+const _1Corinthians = new Book();
+const _2Corinthians = new Book();
+const Galatians = new Book();
+
+const Ephesians = new Book();
+const Philippians = new Book();
+const Colossians = new Book();
+const _1Thessalonians = new Book();
+const _2Thessalonians = new Book();
+
+const _1Timothy = new Book();
+const _2Timothy = new Book();
+const Titus = new Book();
+const Philemon = new Book();
+const Hebrews = new Book();
+
+const James = new Book();
+const _1Peter = new Book();
+const _2Peter = new Book();
+const _1John = new Book();
+const _2John = new Book();
+
+const _3John = new Book();
+const Jude = new Book();
+const Revelation = new Book();
