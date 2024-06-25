4c9f77f3cf47f39fe14e599ed38d627f87046dbd
Nathan Sit <nsit.earth@gmail.com>
2024-06-24 23:43:43 -0700

    removing merge conflicts

--- src/classes/Passage.ts
+++ src/classes/Passage.ts
@@ -1,12 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-<<<<<<< HEAD:src/Book.ts
 class Book<T> {
   prev: Book<T>;
   next: Book<T>;
   chapterCount: number;
   metadata: T[] [];
-=======
-class Passage {
->>>>>>> master:src/classes/Passage.ts
   constructor() { }
 }
