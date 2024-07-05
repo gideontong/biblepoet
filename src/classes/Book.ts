a07edb96390dfea1c977d371aeece5a1d21bb052
Nathan Sit <nsit.earth@gmail.com>
2024-07-04 21:17:37 -0700

    remove colons

--- src/classes/Book.ts
+++ src/classes/Book.ts
@@ -1,11 +1,11 @@
 /**
  * Represents a Book in the Bible
- * @property {string} name: name of the Book
- * @property {Map<string, string>} otherNames: alternative names for Book
- * @property {number} chapterCount: number of chapters in Book
- * @property {Book} prev: the previous Book of the Bible
- * @property {Book} next: the next Book of the Bible
- * @property {object} metadata: data on, but not of Book
+ * @property {string} name name of the Book
+ * @property {Map<string, string>} otherNames alternative names for Book
+ * @property {number} chapterCount number of chapters in Book
+ * @property {Book} prev the previous Book of the Bible
+ * @property {Book} next the next Book of the Bible
+ * @property {object} metadata data about book
  */
 class Book {
   name: string;
