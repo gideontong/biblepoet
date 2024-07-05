550be2d17c9b5f425a8c099570a9d478e34e89e7
Nathan Sit <nsit.earth@gmail.com>
2024-07-04 21:15:10 -0700

    add jsdocs for Book

--- src/classes/Book.ts
+++ src/classes/Book.ts
@@ -1,3 +1,12 @@
+/**
+ * Represents a Book in the Bible
+ * @property {string} name: name of the Book
+ * @property {Map<string, string>} otherNames: alternative names for Book
+ * @property {number} chapterCount: number of chapters in Book
+ * @property {Book} prev: the previous Book of the Bible
+ * @property {Book} next: the next Book of the Bible
+ * @property {object} metadata: data on, but not of Book
+ */
 class Book {
   name: string;
   // TODO: Only allow valid languages
