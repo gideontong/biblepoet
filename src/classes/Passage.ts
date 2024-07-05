81eed0d88f5053977d27552e83cb7525427d3e5b
Gideon Tong <gideon@gideontong.com>
2024-07-05 20:41:25 +0300

    Add support for Passage to contain a series of Reference (#37)

--- src/classes/Passage.ts
+++ src/classes/Passage.ts
@@ -1,5 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class Passage {
+  passages: [Reference];
   metadata: object;
-  constructor() {}
+  constructor() { }
 }
+
+export default Passage;
