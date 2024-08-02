951097f62755d16062bcf15dc95a70ec1790097f
Gideon Tong <gideon@gideontong.com>
2024-08-01 22:02:22 -0700

    Create objects representing the most popular English versions (#18)

---
+++ src/data/versions.en.ts
@@ -0,0 +1,27 @@
+import Version from '../classes/Version';
+
+const ESV2001 = new Version();
+const ESV2007 = new Version();
+const ESV2011 = new Version();
+const ESV2016 = new Version();
+
+const NIV1984 = new Version();
+const NIV2011 = new Version();
+
+const KJV = new Version();
+const NKJV = new Version();
+const NASB = new Version();
+const ASV = new Version();
+
+export default {
+  ESV2001,
+  ESV2007,
+  ESV2011,
+  ESV2016,
+  NIV1984,
+  NIV2011,
+  KJV,
+  NKJV,
+  NASB,
+  ASV,
+};
